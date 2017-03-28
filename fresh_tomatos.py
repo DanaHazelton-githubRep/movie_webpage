6fca261d65cae801660e1135a3c3afdb1f68ba61
D Hazelton <dphaz@bellsouth.net>
2017-03-28 19:05:36 -0400

    Formatting and Comments

--- fresh_tomatos.py
+++ fresh_tomatos.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import webbrowser
 import os
 import re
--- media.py
+++ media.py
@@ -1,50 +1,22 @@
-# Lesson 3.4: Make Classes
-# Mini-Project: Movies Website
-
-# In this file, you will define the class Movie. You could do this
-# directly in entertainment_center.py but many developers keep their
-# class definitions separate from the rest of their code. This also
-# gives you practice importing Python files.
-
-# https://www.udacity.com/course/viewer#!/c-nd000/l-4185678656/m-1013629057
+#!/usr/bin/env python
+# This file defines the class Movie. 
 
 import webbrowser
 
 class Movie():
     """ This class provides a way to store movie related information"""
-    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #class varible-constant so capital letters
+    VALID_RATINGS = ["G", "PG", "PG-13", "R"] #Define  a class varible-constant
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube,VALID_RATINGS):
+    def __init__(self, movie_title, movie_storyline, poster_image, 
+                trailer_youtube,VALID_RATINGS):
         # initialize instance of class Movie
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
         self.rating = VALID_RATINGS
-    def show_trailer(self):
+
+    #open a webbrowser and play movie trailer   
+    def show_trailer(self):#instance method
         webbrowser.open(self.trailer_youtube_url)
 
-class Music():
-    """ This class provides a way to store music related information"""
-
-    def __init__(self,band_name,band_album,album_cover,song_preview):
-        # initialize instance of class Music
-        self.name = band_name
-        self.band_album = band_album
-        self.album_cover = album_cover
-        self.song_preview = song_preview
-
-
-
-
-
-##if __name__ == '__main__':
-##	print 'This program is being run by itself'
-##	
-##else:
-##	print 'I am being imported from another module'
-##	        
-# print (Movie.__module__)
-# print (Movie.__doc__)
-# print (Music.__name__)
-#print (grown_ups.storyline)
